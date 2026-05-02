d8d75596266d48263083f3e3a1707cbfecc3a1ec
Udaya59167 <59167@niels.brock.dk>
2026-05-02 10:27:09 +0200

    Modified

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/books", require("./routes/bookRoutes"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/users", require("./routes/userRoutes"));
 
-// Test route
 app.get("/", (req, res) => {
   res.send("LibraSys backend is running");
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
