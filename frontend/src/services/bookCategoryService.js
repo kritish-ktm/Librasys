45d510634d6a709b2694b7e421cf27579c2bb600
Kritish Upadhyaya <iminati921@gmail.com>
2026-05-05 00:32:33 +0200

    Book category service updated with tooglebookcategory function

---
+++ frontend/src/services/bookCategoryService.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/categories";
+
+export const getCategories = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+export const addCategory = async (data) => {
+  const response = await axios.post(API_URL, data);
+  return response.data;
+};
+
+export const updateCategory = async (id, data) => {
+  const response = await axios.put(`${API_URL}/${id}`, data);
+  return response.data;
+};
+
+export const deleteCategory = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`);
+  return response.data;
+};
+
+export const toggleCategoryStatus = async (id, currentStatus) => {
+  const response = await axios.put(`${API_URL}/${id}`, {
+    status: !currentStatus,
+  });
+  return response.data;
+};
